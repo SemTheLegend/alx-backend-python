3c9ea6b605a7e2bcba8f89a2b3fb8ac4829f4614
SemTheLegend <semmathe@gmail.com>
2023-03-30 12:06:01 +0000

    3-Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module returns a string representation function."""
+
+
+def to_str(n: float) -> str:
+    """Returns a string conversion of a float."""
+
+    return str(n)
