24dab7bbe327979b528ed61eb8b8811f48bb61ba
SemTheLegend <semmathe@gmail.com>
2023-03-30 11:58:33 +0000

    task 2 - Basic Annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module defines a floor function."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float."""
+
+    return math.floor(n)
