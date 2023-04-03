c834c118a57f17d91bf1d6f6edf0198f78c40e1a
SemTheLegend <semmathe@gmail.com>
2023-04-03 17:08:27 +0000

    3. Tasks

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that measures the runtime
+
+Of an asynchronous function.
+"""
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Returns the total execution time for this function."""
+
+    start = time.perf_counter()
+    res = await wait_n(n, max_delay)
+    total_time = time.perf_counter() - start
+    
+
+    return res
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module defines a function that returns a asyncio Task ."""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """Returns a `asyncio.Task`."""
+
+    res = asyncio.create_task(wait_random(max_delay))
+    return res
